aa96d9e4df352973834b94610a3ec07390297d0a
nohminhyeok <kygh1488@naver.com>
2025-08-08 20:44:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 344 ms, Memory: 88644 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int count = 0;
+        int num = 666;
+
+        while (true) {
+            if (String.valueOf(num).contains("666")) {
+                count++;
+                if (count == n) {
+                    System.out.println(num);
+                    return;
+                }
+            }
+            num++;
+        }
+    }
+}
