379c7e7e732449498d7e6932ea94fce7126b78a2
nohminhyeok <kygh1488@naver.com>
2025-08-11 09:40:11 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[5];
+		int sum = 0;
+		
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+
+		int avg = sum/arr.length;
+		int near = arr[2];
+		
+		System.out.println(avg);
+		System.out.println(near);
+    }
+}
